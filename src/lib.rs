5da474c31c1888f3a60fa4c6e02bb3951f2d7223
Pierre <p.niberon@outlook.fr>
2022-11-06 10:05:39 +0400

    refactoring and housekeeping

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
+}
+
+#[get("/echo/{req}")]
+async fn echo(req: web::Path<String>) -> impl Responder {
+    format!("{}", req)
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+pub async fn run() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(health_check))
+            .route("/hello", web::get().to(|| async { "Hello World!" }))
+            .service(greet)
+            .service(echo)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,6 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use zero2prod::run;
 
-#[get("/hello/{name}")]
-async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name}!")
-}
-
-#[get("/echo/{req}")]
-async fn echo(req: web::Path<String>) -> impl Responder {
-    format!("{}", req)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
-
-#[actix_web::main] // or #[tokio::main]
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(health_check))
-            .route("/hello", web::get().to(|| async { "Hello World!" }))
-            .service(greet)
-            .service(echo)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    run().await
 }
