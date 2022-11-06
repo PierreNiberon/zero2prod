7276789b3620326447d245e533512b8949b2ed52
Pierre <p.niberon@outlook.fr>
2022-11-06 20:22:34 +0400

    adding a subscription endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,12 @@ use actix_web::dev::Server;
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
     format!("Hello {name}!")
@@ -16,10 +22,15 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
             .route("/hello", web::get().to(|| async { "Hello World!" }))
             .service(greet)
             .service(echo)
