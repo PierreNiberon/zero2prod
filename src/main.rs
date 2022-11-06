ff363c4f8d3e18d94f209ff33c689a68636a01a4
Pierre <p.niberon@outlook.fr>
2022-11-06 09:14:08 +0400

    Added an echo handler

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,18 @@ async fn greet(name: web::Path<String>) -> impl Responder {
     format!("Hello {name}!")
 }
 
+#[get("/echo/{req}")]
+async fn echo(req: web::Path<String>) -> impl Responder {
+    format!("{}", req)
+}
+
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/hello", web::get().to(|| async { "Hello World!" }))
             .service(greet)
+            .service(echo)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
