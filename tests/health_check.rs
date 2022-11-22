ac340643c627017b7b23b6c85559b5f17959794b
Pierre <p.niberon@outlook.fr>
2022-11-22 22:12:10 +0400

    reworked CI and test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -8,11 +8,12 @@ use zero2prod::startup::run;
 // We are also running tests, so it is not worth it to propagate errors:
 // if we fail to perform the required setup we can just panic and crash
 // all the things.
-fn spawn_app() -> String {
+
+fn spawn_app(connection: PgConnection) -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     // We retrieve the port assigned to us by the OS
     let port = listener.local_addr().unwrap().port();
-    let server = run(listener).expect("Failed to bind address");
+    let server = run(listener, connection).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     // We return the application address to the caller!
     format!("http://127.0.0.1:{}", port)
@@ -26,8 +27,12 @@ fn spawn_app() -> String {
 // `cargo expand --test health_check` (<- name of the test file)
 #[tokio::test]
 async fn health_check_works() {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failure to connect to DB");
     // Arrange
-    let adress = spawn_app();
+    let adress = spawn_app(connection);
     // We need to bring in `reqwest`
     // to perform HTTP requests against our application.
     let client = reqwest::Client::new();
@@ -48,10 +53,10 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_string = configuration.database.connection_string();
-    let mut connection = PgConnection::connect(&connection_string)
+    let connection = PgConnection::connect(&connection_string)
         .await
         .expect("Failed to connect to postgres DB");
-    let app_address = spawn_app();
+    let app_address = spawn_app(connection);
     let client = reqwest::Client::new();
     // Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -64,9 +69,12 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request.");
     // Assert
     assert_eq!(200, response.status().as_u16());
-
+    // using a reconnection to the DB because of Rust borrowing system...
+    let mut reconnection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to postgres DB");
     let saved = sqlx::query!("SELECT name, email FROM subscriptions")
-        .fetch_one(&mut connection)
+        .fetch_one(&mut reconnection)
         .await
         .expect("Failed to fetch saved subscription.");
 
@@ -76,8 +84,12 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
 #[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failure to connect to DB");
     // Arrange
-    let app_address = spawn_app();
+    let app_address = spawn_app(connection);
     let client = reqwest::Client::new();
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
