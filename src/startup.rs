9d307166fc75d61e692e9c5e37ecadcfc7d273d2
Pierre <p.niberon@outlook.fr>
2022-11-28 14:40:59 +0400

    added logs

--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,6 @@
 use crate::routes::{health_check::health_check, subscriptions::subscribe};
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -17,6 +18,7 @@ pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io:
     // the app_data is for persistence in db
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .route("/hello", web::get().to(|| async { "Hello World!" }))
