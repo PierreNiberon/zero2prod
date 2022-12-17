649c23b6c21ac523f922eb606bd1671bcaff8008
Pierre <p.niberon@outlook.fr>
2022-12-17 16:04:47 +0400

    clippy clean

--- src/configuration.rs
+++ src/configuration.rs
@@ -41,7 +41,7 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
             configuration_directory.join("base.yaml"),
         ))
         .add_source(config::File::from(
-            configuration_directory.join(&environment_filename),
+            configuration_directory.join(environment_filename),
         ))
         .build()?;
     // Try to convert the configuration values it read into
